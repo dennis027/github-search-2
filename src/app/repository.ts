5e9c8cf3fd3523ac058cbc1bb5415eda935b5382
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-15 13:21:03 +0300

    adding constructors

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,9 @@
 export class Repository {
+    constructor (
+        public name:string, 
+        public description:string,
+        public updated_at:Date,
+        public clone_url:string,
+        public language: string
+    ){}
 }
