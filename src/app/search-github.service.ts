0137f324292ed0480c2718806180e5c065017ed3
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-15 15:21:16 +0300

    adding a service class

---
+++ src/app/search-github.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import{User} from './user';
+import{Repository} from './repository';
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchGithubService {
+user!: User;
+repository!: Repository;
+repoData! : [];
+  constructor(private http: HttpClient) {
+    this.user = new User("",0,"","",new Date());
+   }
+}
