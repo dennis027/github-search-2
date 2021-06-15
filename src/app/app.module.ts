1279d1f01422f6f98398699384bc89b946d4a63f
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-16 00:50:25 +0300

    main app module file in my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http'; 
 import { AppComponent } from './app.component';
-import { GitFormComponent } from './git-form/git-form.component';
-import { GitResultsComponent } from './git-results/git-results.component';
+import { GitRoutingModule } from './git-routing.module'; 
+import { GitFormComponent } from './git-form/git-form.component'; 
+import { GitSearchResultsComponent } from './git-search-results/git-search-results.component';
+import { SearchGithubService } from './search-github.service'; 
+import { DateFormatPipe } from './date-format.pipe'; 
+import { CloneHighlighterDirective } from './clone-highlighter.directive'; 
+import { DaysCountPipe } from './days-count.pipe'; 
+import { TimeAgoPipe } from 'time-ago-pipe'; 
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    GitFormComponent,
-    GitResultsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   
+   declarations: [
+      AppComponent,
+      GitFormComponent,
+      GitSearchResultsComponent,
+      DateFormatPipe,
+      CloneHighlighterDirective,
+      DaysCountPipe,
+     
+      TimeAgoPipe
+   ], 
+
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpClientModule,
+      GitRoutingModule
+   ],
+   providers: [SearchGithubService], 
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
